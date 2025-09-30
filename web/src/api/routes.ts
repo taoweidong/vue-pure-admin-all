acc5142e556f8520c8c50f5c57a8d44065222044
User <user@example.com>
2025-09-30 23:27:31 +0800

    feat: 优化前端配置和API工具函数

--- web/src/api/routes.ts
+++ web/src/api/routes.ts
@@ -1,3 +1,4 @@
+import { baseUrlApi } from "./utils";
 import { http } from "@/utils/http";
 
 type Result = {
@@ -6,5 +7,5 @@ type Result = {
 };
 
 export const getAsyncRoutes = () => {
-  return http.request<Result>("get", "/get-async-routes");
+  return http.request<Result>("get", baseUrlApi("/get-async-routes"));
 };
